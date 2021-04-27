ae0b571ca0da2f1ee609747a2d8da1cea7a1101b
Randy Steele <steelerx155@Gmail.com>
2021-04-27 06:44:51 -0400

    added styling to login.css

--- .history/src/Login_20210427063944.js
+++ .history/src/Login_20210427063944.js
@@ -5,8 +5,9 @@ class Login extends Component {
     render() {
         return (
             <div class="login">
+                <h1>Login</h1>
                 <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
-                <a>Login with Spotify</a>
+                <button>Login with Spotify</button>
 
             </div>
         );
--- .history/src/Login_20210427063954.js
+++ .history/src/Login_20210427063954.js
@@ -6,7 +6,7 @@ class Login extends Component {
         return (
             <div class="login">
                 <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
-                <a>Login with Spotify</a>
+                <button>Login with Spotify</button>
 
             </div>
         );
